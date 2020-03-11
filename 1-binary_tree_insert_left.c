20c4bc995c6fcd9c676926c6f63b45b0ef8d3eb2
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-11 22:43:55 +0000

    [Fix]: Betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,24 +7,24 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-        if (!parent)
-                return (NULL);
+	if (!parent)
+		return (NULL);
 
-        new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 
-        if (!new_node)
-                return (NULL);
+	if (!new_node)
+		return (NULL);
 
-        new_node->parent = parent;
-        new_node->n = value;
-        new_node->right = NULL;
-        new_node->left = parent->left;
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->right = NULL;
+	new_node->left = parent->left;
 
-        if (new_node->left != NULL)
-                parent->left->parent = new_node;
+	if (new_node->left != NULL)
+		parent->left->parent = new_node;
 
-        parent->left = new_node;
-        return (new_node);
+	parent->left = new_node;
+	return (new_node);
 }
