7dc28534f651802a0daae69132d394383090a8e7
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-11 22:35:00 +0000

    [Fix]: Conditions

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,7 +7,12 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (!new_node)
 		return (NULL);
@@ -15,8 +20,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->right = parent->right;
+
 	if (new_node->right != NULL)
 		new_node->right->parent = new_node;
+
 	parent->right = new_node;
 	return (new_node);
 }
